c57167837de0cb83b65037a44f4c24257400e323
Louise <t7861231@gmail.com>
2018-09-17 17:04:22 +0800

    add basic read.py

---
+++ read_reviews.py
@@ -0,0 +1,12 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 10000 ==0:    #%求餘數 
+			print(len(data))
+print(len(data))
+print(data[0])  #印出第一筆資料
+print('----------')
+print(data[1])  #印出第二筆資料
